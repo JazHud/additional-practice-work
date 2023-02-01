a51c29ab52d93e40f77d50d2340158f45373c7ad
JazMac <jazmine.n.hudson@gmail.com>
2023-02-01 10:47:16 -0600

    submit button function

--- js/daycare-roster.js
+++ js/daycare-roster.js
@@ -1,7 +1,30 @@
 
 const btn = document.getElementById('submit-child-info');
+
 let guardName = document.getElementById('guardian-name');
 let childName = document.getElementById('childname');
 let phoneNum = document.getElementById('phone-number');
 
 
+
+let inputList = document.getElementById('kidlist');
+
+// function showlist(){
+//     let showkids = guardName.value + " " + childName.value +" " + phoneNum.value;
+//     console.log(showkids);
+//
+//     inputList.append = showkids;
+//
+// }
+//
+// console.log(showlist());
+// btn.addEventListener('click', showlist);
+
+//to loop through kiddos
+// allKids.forEach(childName =>
+//     console.log(childName));
+
+btn.addEventListener('click', function (){
+    let showkids = guardName.value + " " + childName.value +" " + phoneNum.value + "\n";
+    inputList.append(showkids);
+})
