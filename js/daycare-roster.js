95cbdf7ab4680a62f878eada38106c2dbc875d20
JazMac <jazmine.n.hudson@gmail.com>
2023-02-01 11:34:16 -0600

    converted show of kids into a table

--- js/daycare-roster.js
+++ js/daycare-roster.js
@@ -1,30 +1,31 @@
-
+function addKid(){
 const btn = document.getElementById('submit-child-info');
 
-let guardName = document.getElementById('guardian-name');
-let childName = document.getElementById('childname');
-let phoneNum = document.getElementById('phone-number');
+// let guardName = document.getElementById('guardian-name');
+// let childName = document.getElementById('childname');
+// let phoneNum = document.getElementById('phone-number');
 
+let tableBody = document.getElementById('table-body');
+let tdGuard = document.createElement('td');
+let tdChild = document.createElement('td');
+let tdphone = document.createElement('td');
 
+let tr = document.createElement('tr');
 
-let inputList = document.getElementById('kidlist');
+tdGuard.innerHTML = document.getElementById('guardian-name').value;
+tdChild.innerHTML = document.getElementById('childname').value;
+tdphone.innerHTML = document.getElementById('phone-number').value;
 
-// function showlist(){
-//     let showkids = guardName.value + " " + childName.value +" " + phoneNum.value;
-//     console.log(showkids);
-//
-//     inputList.append = showkids;
-//
-// }
-//
-// console.log(showlist());
-// btn.addEventListener('click', showlist);
+tr.appendChild(tdGuard);
+tr.appendChild(tdChild);
+tr.appendChild(tdphone);
 
-//to loop through kiddos
-// allKids.forEach(childName =>
-//     console.log(childName));
+tableBody.appendChild(tr);
+}
+// let inputList = document.getElementById('kidlist');
 
-btn.addEventListener('click', function (){
-    let showkids = guardName.value + " " + childName.value +" " + phoneNum.value + "\n";
-    inputList.append(showkids);
-})
+// btn.addEventListener('click', function (){
+//     let showkids = guardName.value + " " + childName.value +" " + phoneNum.value + "\n";
+//     inputList.append(showkids);
+// });
+
