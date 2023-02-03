65daf05f0f4909d0f0a5d6aafe96abda9e706117
JazMac <jazmine.n.hudson@gmail.com>
2023-02-02 20:35:40 -0600

    cleaned up code

--- js/daycare-roster.js
+++ js/daycare-roster.js
@@ -1,19 +1,13 @@
 function addKid(){
 
-
-// let guardName = document.getElementById('guardian-name');
-// let childName = document.getElementById('childname');
-// let phoneNum = document.getElementById('phone-number');
-
 let tableBody = document.getElementById('table-body');
 let tdGuard = document.createElement('td');
 let tdChild = document.createElement('td');
 let tdphone = document.createElement('td');
-
 let tr = document.createElement('tr');
 
 tdGuard.innerHTML = document.getElementById('guardian-name').value;
-tdChild.innerHTML = document.getElementById('childname').value;
+tdChild.innerHTML = document.getElementById('child-name').value;
 tdphone.innerHTML = document.getElementById('phone-number').value;
 
 tr.appendChild(tdGuard);
@@ -21,29 +15,17 @@ tr.appendChild(tdChild);
 tr.appendChild(tdphone);
 
 tableBody.appendChild(tr);
-}
 
-const btn = document.getElementById('submit-child-info');
-
-////making the child's information into an object
-// let childName = document.getElementById('childname').value;
-// let childGuard = document.getElementById('guardian-name').value;
-// let childNum = document.getElementById('phone-number').value;
-
-
-
-// let inputList = document.getElementById('kidlist');
-
-btn.addEventListener('click', function (){
-    let listOfKids = []
     let newKid = {
-        name: document.getElementById('childname').value,
+        name: document.getElementById('child-name').value,
         guardian: document.getElementById('guardian-name').value,
         phoneNum: document.getElementById('phone-number').value
     };
-    listOfKids.push(newKid);
+
     console.log(newKid);
-    console.log(listOfKids);
-    // inputList.append(showkids);
-});
+}
+
+const btn = document.getElementById('submit-child-info');
+btn.addEventListener('click', addKid);
+
 
