6eb88cc8a5b5c05d2b86b20b580270d3b0747f23
JazMac <jazmine.n.hudson@gmail.com>
2023-02-07 14:53:03 -0600

    need to populate table and clear it automaticlly

--- js/daycare-roster.js
+++ js/daycare-roster.js
@@ -5,7 +5,7 @@ let children = [];
 
 function addKid() {
 
-    getData();
+    // getData();
 
     children.push({
         id: Date.now().toString(),
@@ -17,14 +17,14 @@ function addKid() {
     localStorage.setItem('Roster', JSON.stringify(children));
 }
 
-function getData(){
-    let dataString = localStorage.getItem('Roster');
-    if (dataString != null)
-        children = JSON.parse(dataString);
-}
+// function getData(){
+//     let dataString = localStorage.getItem('Roster');
+//     if (dataString != null)
+//         children = JSON.parse(dataString);
+// }
 
 // to input the tables via loop
-function rosterUpdate() {
+function rosterUpdate(){
 
     let childrenUpdate = [];
     childrenUpdate = JSON.parse(localStorage.getItem('Roster'));
@@ -44,28 +44,9 @@ function rosterUpdate() {
         phoneRow.innerHTML = childrenUpdate[i].phoneNum;
     }
 
-};
+}
 
 rosterUpdate();
-
-    // let tableBody = document.getElementById('table-body');
-    // let tdGuard = document.createElement('td');
-    // let tdChild = document.createElement('td');
-    // let tdphone = document.createElement('td');
-    // let tr = document.createElement('tr');
-    //
-    // tdGuard.innerHTML = document.getElementById('guardian-name').value;
-    // tdChild.innerHTML = document.getElementById('child-name').value;
-    // tdphone.innerHTML = document.getElementById('phone-number').value;
-    //
-    // tr.appendChild(tdGuard);
-    // tr.appendChild(tdChild);
-    // tr.appendChild(tdphone);
-    //
-    // tableBody.appendChild(tr);
-
-
-
 
 const btn = document.getElementById('submit-child-info');
 btn.addEventListener('click', addKid);
